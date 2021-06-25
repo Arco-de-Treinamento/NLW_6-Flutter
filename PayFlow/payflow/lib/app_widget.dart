3c7639086c58820a539c54ea5d11bce910dc998d
JosManoel <josmanoelfreitas2012@gmail.com>
2021-06-25 20:59:53 -0300

    add scanner page

--- PayFlow/payflow/lib/app_widget.dart
+++ PayFlow/payflow/lib/app_widget.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+
+  // Travando orientação da tela
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
+      theme: ThemeData(
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
       initialRoute: "/splash",
 
-    // Rotas nomeadas
+      // Rotas nomeadas
       routes: {
         "/splash": (context) => SplashPage(),
-        "/login":  (context) => LoginPage(),
-        "/home":   (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
